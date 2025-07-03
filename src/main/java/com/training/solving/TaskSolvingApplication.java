104d4b841ab0b4cb6d780b3985cee08108e37873
Andrei <agafonov.andrei.val@gmail.com>
2025-07-03 10:26:02 +0200

    Create initial structure

---
+++ src/main/java/com/training/solving/TaskSolvingApplication.java
@@ -0,0 +1,13 @@
+package com.training.solving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSolvingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskSolvingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/training/solving/TaskSolvingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.solving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSolvingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
